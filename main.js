0784d7bb4b0213c58deb2d4b8353a947d18b35ab
Roque Martin Minaglia <minagliaroquemartin@gmail.com>
2023-05-24 20:02:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = document.getElementById('typewriter');
+
+const typewriter = new Typewriter(app, {
+    loop: true,
+    delay:110
+});
+
+typewriter
+    .typeString('Soy un "Desarrollador Web FullStack"')
+    .pauseFor(100)
+    .start();
